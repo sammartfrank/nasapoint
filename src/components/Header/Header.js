cc60c00928d3bac00f78951cd82948e7605f6deb
sammartfrank <franksammartino7@gmail.com>
2019-07-04 20:42:12 -0300

    thursday6

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ const Header = ({state}) => {
  return (
   <div className={ `header  ${ state ? 'sticky' : '' }` }>
   	<div className={`redline ${ state ? 'go' : ''}`}></div>
+  	<div className={`darksq ${ state ? 'fish' : ''}`}></div>
     <img className="logo" src={ig} alt=""/>
    <h1>NASA API POINT</h1>
   </div>
