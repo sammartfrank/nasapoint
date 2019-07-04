a734e4bcd0e61b5ed678c7d71315528d8ccba8ec
sammartfrank <franksammartino7@gmail.com>
2019-07-04 16:26:39 -0300

    tring

--- webpack.config.js
+++ webpack.config.js
@@ -9,48 +9,48 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js'
   },
- module: {
-  rules: [
-   {
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: {
-     loader: "babel-loader"
-    }
-   },
-   {
-    test: /\.html$/,
-    use: [
-      {
-       loader: "html-loader"
+   module: {
+    rules: [
+     {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+       loader: "babel-loader"
       }
+     },
+     {
+      test: /\.html$/,
+      use: [
+        {
+         loader: "html-loader"
+        }
+      ]
+     },
+     {
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '../',
+            hmr: process.env.NODE_ENV === 'development',
+          },
+        },
+        'css-loader',
+      ],
+    },
+    {
+      test: /\.(jpg|jpeg|png|gif|ico)$/,
+      exclude: /node_modules/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'images/',
+        publicPath: 'dist/images'
+      }
+    }
     ]
    },
-   {
-    test: /\.css$/,
-    use: [
-      {
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          publicPath: '../',
-          hmr: process.env.NODE_ENV === 'development',
-        },
-      },
-      'css-loader',
-    ],
-  },
-  {
-    test: /\.(jpg|jpeg|png|gif|ico)$/,
-    exclude: /node_modules/,
-    loader: 'file-loader',
-    options: {
-      name: '[name].[ext]',
-      outputPath: 'images/',
-      publicPath: 'dist/images'
-    }
-  }
-  ]
- },
   plugins: [
     new HtmlWebpackPlugin({
      template: "./public/index.html",
