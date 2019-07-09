1e2b1e7ce2900dfc1068f412afbbf7ad9c717cc8
sammartfrank <franksammartino7@gmail.com>
2019-07-09 18:16:19 -0300

    tuesday7

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,13 @@ import './Header.css'
 const Header = ({state}) => {
  return (
   <div className={ `header  ${ state ? 'sticky' : '' }` }>
-  	<div className={`redline ${ state ? 'go' : ''}`}></div>
+  	<div className={`blueline ${ state ? 'go' : ''}`}></div>
   	<div className={`darksq ${ state ? 'fish' : ''}`}></div>
+  	<div className={`stripes ${ state ? 'going' : ''}`}>
+  		<div className='first'></div>
+  		<div className='second'></div>
+  		<div className='third'></div>
+  	</div>
     <img  className={`earth ${ state ? 'up' : ''}`} src={earth} alt="Earth"/>
     <img className="logo" src={ig} alt="NasaLogo"/>
    <h1>NASA API POINT</h1>
